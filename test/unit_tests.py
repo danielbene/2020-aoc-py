78f468acfe90e166f57c4d6a65b6c2e8285e1746
Dániel Bene <danielbene@protonmail.com>
2020-12-04 15:50:30 +0100

    Add solution to part1

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -59,3 +59,23 @@ class Test(TestCase):
             '.#..#...#.#']
         correct_answer = 336
         self.assertTrue(multiplied_tree_probabilities(input_list) == correct_answer)
+
+    def test_num_of_valid_passports(self):
+        from src.day4_1 import num_of_valid_passports
+        input_list = [
+            'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n',
+            'byr:1937 iyr:2017 cid:147 hgt:183cm\n',
+            '\n',
+            'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\n',
+            'hcl:#cfa07d byr:1929\n',
+            '\n',
+            'hcl:#ae17e1 iyr:2013\n',
+            'eyr:2024\n',
+            'ecl:brn pid:760753108 byr:1931\n',
+            'hgt:179cm\n',
+            '\n',
+            'hcl:#cfa07d eyr:2025 pid:166559648\n',
+            'iyr:2011 ecl:brn hgt:59in\n',
+            '\n']
+        correct_answer = 2
+        self.assertTrue(num_of_valid_passports(input_list) == correct_answer)
