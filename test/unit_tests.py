88e72c964046e14ab242d2e6114421e8c96a300d
Dániel Bene <danielbene@protonmail.com>
2020-12-04 18:40:57 +0100

    Add solution to part2

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -78,3 +78,37 @@ class Test(TestCase):
             'iyr:2011 ecl:brn hgt:59in\n']
         correct_answer = 2
         self.assertTrue(num_of_valid_passports(input_list) == correct_answer)
+
+    def test_num_of_valid_passports_by_rules(self):
+        from src.day4_2 import num_of_valid_passports_by_rules
+        invalid_input_list = [
+            'eyr:1972 cid:100\n',
+            'hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\n',
+            '\n',
+            'iyr:2019\n',
+            'hcl:#602927 eyr:1967 hgt:170cm\n',
+            'ecl:grn pid:012533040 byr:1946\n',
+            '\n',
+            'hcl:dab227 iyr:2012\n',
+            'ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\n',
+            '\n',
+            'hgt:59cm ecl:zzz\n',
+            'eyr:2038 hcl:74454a iyr:2023\n',
+            'pid:3556412378 byr:2007\n']
+        valid_input_list = [
+            'pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\n',
+            'hcl:#623a2f\n',
+            '\n',
+            'eyr:2029 ecl:blu cid:129 byr:1989\n',
+            'iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\n',
+            '\n',
+            'hcl:#888785\n',
+            'hgt:164cm byr:2001 iyr:2015 cid:88\n',
+            'pid:545766238 ecl:hzl\n',
+            'eyr:2022\n',
+            '\n',
+            'iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719\n']
+        with self.subTest():
+            self.assertEqual(num_of_valid_passports_by_rules(invalid_input_list), 0)
+        with self.subTest():
+            self.assertEqual(num_of_valid_passports_by_rules(valid_input_list), 4)
