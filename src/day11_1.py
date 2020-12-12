ee129d2e67dcf095233837a01479cec4caab0293
Dániel Bene <danielbene@protonmail.com>
2020-12-12 15:42:30 +0100

    Add solution to day11 part1

--- src/day11_1.py
+++ src/day11_1.py
@@ -1,4 +1,5 @@
 from util import iohandler
+from copy import deepcopy
 
 # --- solution ---
 
@@ -16,36 +17,51 @@ directions = {
 
 def get_occupied_seats(input_file):
     waiting_area = [list(row.strip()) for row in input_file]
-    mutator = waiting_area.copy()
-    width = len(waiting_area[0])
-    height = len(waiting_area)
+    mutator = deepcopy(waiting_area)  # deepcopy needed if mutable inner object/collections present
+    width, height = len(waiting_area[0]), len(waiting_area)
+    stable, is_odd_round = False, True
 
-    for row_index in range(len(waiting_area)):
-        for position_index in range(len(waiting_area[row_index])):
-            if waiting_area[int(row_index)][int(position_index)] != '.' and \
-                    count_occupied_adjacency(waiting_area, int(row_index), int(position_index), width, height) == 0:
-                mutator[int(row_index)][int(position_index)] = '#'
+    while not stable:
+        for row_index in range(height):
+            for position_index in range(width):
+                row, pos = int(row_index), int(position_index)
+                marker = waiting_area[row][pos]
+                if marker == '.':
+                    continue
 
-    waiting_area = mutator.copy()
+                adj_population = count_occupied_adjacency(waiting_area, row, pos, width, height)
+                if is_odd_round:
+                    if adj_population == 0:
+                        mutator[row][pos] = '#'
+                else:
+                    if adj_population >= 4:
+                        mutator[row][pos] = 'L'
 
-    print(mutator)
+        is_odd_round = not is_odd_round
+        if waiting_area == mutator:
+            stable = True
+        else:
+            waiting_area = deepcopy(mutator)
 
-    return ''
+    return count_in_collection(waiting_area, '#')
 
 
 def count_occupied_adjacency(waiting_area, row_index, position_index, width, height):
     occupied_count = 0
     for d in directions.values():
-        if width < position_index + d[0] < 0:
-            continue
-        elif height < row_index + d[1] < 0:
+        row = row_index + d[1]
+        pos = position_index + d[0]
+        if pos < 0 or pos >= width or row < 0 or row >= height:
             continue
 
-        if waiting_area[row_index][position_index] == '#':
+        if waiting_area[row][pos] == '#':
             occupied_count += 1
 
     return occupied_count
 
+
+def count_in_collection(coll, ch):
+    return sum([row.count(ch) for row in coll])
 
 # --- solution ---
 
