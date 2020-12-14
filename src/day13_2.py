a3cb31312754cbf1f398a4f4b7d891a461a16618
Dániel Bene <danielbene@protonmail.com>
2020-12-14 18:02:38 +0100

    Add solution to part2

--- src/day13_2.py
+++ src/day13_2.py
@@ -1,5 +1,4 @@
 from util import iohandler
-import time
 from functools import reduce
 
 # --- solution ---
@@ -9,47 +8,45 @@ def init(input_file):
     return [str(row.strip()) for row in input_file][1]
 
 
-# refactor based on Chinese Remainder Theory
+# based on Chinese Remainder Theory - i dont like math theories :,)
 # https://fangya.medium.com/chinese-remainder-theorem-with-python-a483de81fbb8
 def get_earliest_matching_departs(data_list):
     bus_ids = [str(bus_id) for bus_id in data_list.split(',')]
     ids_by_delay = dict()
     for index in range(len(bus_ids)):
         bus_id = bus_ids[index]
-        if bus_id != 'x':
-            ids_by_delay.update({index: int(bus_id)})
+        if bus_id != 'x' and index != 0:
+            ids_by_delay.update({int(bus_id): int(bus_id) - index})
+        elif index == 0:
+            ids_by_delay.update({int(bus_id): 0})
 
-    start = time.time()
-    ret = 0
-
-    for num in range(100000000000000, lcm(ids_by_delay.values()), ids_by_delay.pop(0)):
-
-        if time.time() > start + 60:
-            print(time.strftime('%X') + " - Processed nums: " + str(num))
-            start = time.time()
-
-        found = True
-        for key in ids_by_delay.keys():
-            if num % ids_by_delay.get(key) != ids_by_delay.get(key) - key:
-                found = False
-                break
-
-        if found:
-            ret = num
-            break
-
-    print('FINISHED ' + str(ret))
-    return ret
+    return chinese_remainder(ids_by_delay.keys(), ids_by_delay.values())
 
 
-# least common multiple
-# this is built in after py 3.9 (math.lcm(integers))
-def lcm(num_list):
-    multi = 1
-    for num in num_list:
-        multi *= num
+def chinese_remainder(n, a):  # n - mod (bus_id), a - remainder (ts delay)
+    sum_val = 0
+    prod = reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum_val += a_i * mul_inv(p, n_i) * p
+    return sum_val % prod
 
-    return abs(multi)
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1:
+        return 1
+
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+
+    if x1 < 0:
+        x1 += b0
+
+    return x1
 
 
 # --- solution ---
